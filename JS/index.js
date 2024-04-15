34a54135d1faefb62a9bd8f87a508faa12c4caa1
tema <35255039+spadre@users.noreply.github.com>
2024-04-15 11:45:25 +0300

    added dot button and user now can calculate with decimal numbers

--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,38 @@
 var numA =0, numB = 0, operator = "", result=0;
+document.querySelector("#operationInput").value = '';
 
 function addNum(numA,numB){
-    return numA+numB;
+    var functioResult = numA+numB;
+    parseFloatNew(functioResult);
+    return functioResult;
 }
 
 function substarctNum(numA,numB){
-    return numA-numB;
+    var functioResult = numA-numB;
+    parseFloatNew(functioResult);
+    return functioResult;
 }
 
 function multiplyNum(numA,numB){
-    return numA*numB;
+    var functioResult = numA*numB;
+    parseFloatNew(functioResult);
+    return functioResult;
 }
 
 function divideNum(numA,numB){
-    return numA/numB;
+    if(numB == 0){
+        return document.querySelector("#operationInput").value = "Cannot divide with zero";
+    }
+    else {
+    var functioResult = numA/numB;
+    parseFloatNew(functioResult);
+    return functioResult;
+    }
+
 }
+
+
+
 
 function operate(numA,operator,numB){
 
@@ -34,30 +52,47 @@ function operate(numA,operator,numB){
 }
 
 function clearAll(){
-    document.querySelector("#operationInput").textContent = "";
+    document.querySelector("#operationInput").value = "";
     numA = 0;
     numB = 0;
     result = 0;
+    document.querySelector("#dot").disabled = false;
 }
 
-
+// Number buttons
 const numberBtns = document.querySelectorAll("div.calculatorNumbers > button");
 numberBtns.forEach((button) => {
 
     button.addEventListener("click", ()=>{
-        document.querySelector("#operationInput").textContent += button.textContent; 
-        var inputField = document.querySelector("#operationInput").textContent;
+        document.querySelector("#operationInput").value += button.textContent; 
+        var inputField = document.querySelector("#operationInput").value;
        
         if(inputField.includes("*")||inputField.includes("/")||inputField.includes("-")||inputField.includes("+")){
             numB += button.textContent;
-            console.log("numA: "+numA+"\nnumB: "+numB);
         }
         else{ 
             numA += button.textContent;
-            console.log("numA: "+numA+"\nnumB: "+numB);
+            
         }
     });
     
+});
+
+// Dot button
+
+const dotBtn = document.querySelector("#dot")
+dotBtn.addEventListener("click",()=>{
+
+    var regEx = new RegExp("[.]");
+    
+    if(regEx.test(numA.toString())){
+        document.querySelector("#dot").disabled = true;
+    }
+    else if(regEx.test(numB.toString())){
+        document.querySelector("#dot").disabled = true;
+    }
+    else{document.querySelector("#dot").disabled = false;}
+
 });
 
 
@@ -67,23 +102,30 @@ const operationBtns = document.querySelectorAll("div.calculatorOperations > .ope
 operationBtns.forEach((button) => {
     
     button.addEventListener("click", ()=>{
-        var inputField = document.querySelector("#operationInput").textContent;
+        var inputField = document.querySelector("#operationInput").value;
 
         // checks if <div> with "operationInput" id has any operation already and if it has then calculates numbers. 
         if(inputField.includes("*")||inputField.includes("/")||inputField.includes("-")||inputField.includes("+")){
-            document.querySelector("#operationInput").textContent = "";
-            result = operate(parseInt(numA),operator,parseInt(numB));
+            document.querySelector("#operationInput").value = "";
+            result = operate(parseFloat(numA),operator,parseFloat(numB));
             numA = result;
-            document.querySelector("#operationInput").textContent += result;
-            document.querySelector("#operationInput").textContent += button.textContent;
+            //parseFloatNew(numA);
+
+            document.querySelector("#operationInput").value += numA;
+            document.querySelector("#operationInput").value += button.textContent;
+
+            console.log("NumA: "+numA+"\nNumB: "+numB+"\nResult: "+result)
             numB = 0;
             operator = button.textContent;
-
+            console.log("NumA: "+numA+"\nNumB: "+numB+"\nResult: "+result)
+            document.querySelector("#dot").disabled = false;
         }
         else{
-            document.querySelector("#operationInput").textContent += button.textContent;
+            document.querySelector("#operationInput").value += button.textContent;
             operator = button.textContent;
+            document.querySelector("#dot").disabled = false;
         }
+
     });
     
 });
@@ -96,9 +138,31 @@ clearButton.addEventListener("click", clearAll);
 // Equal button uses "operate" function to calculate numbers that are in "operationInput" <div> and displays result
 const equalButton = document.querySelector("div.calculatorOperations > #equal");
 equalButton.addEventListener("click",() => {
-    result = operate(parseInt(numA),operator,parseInt(numB));
-    document.querySelector("#operationInput").textContent = result;
+    result = operate(parseFloat(numA),operator,parseFloat(numB));
+    document.querySelector("#operationInput").value = result;
+
+    numA = 0;
     numA = result;
     numB = 0;
     console.log(result)
 });
+
+
+// numbers using only two decimals. toFixed() didn't work properly
+function parseFloatNew(number){
+    number = number.toString();
+    number = number.slice(0,(number.indexOf(".")) + 2 + 1);
+    return Number(number);
+}
+
+
+
+test();
+function test(){
+    
+    var testi1 = "415142.51",testi2 = 4512, tulos =0;
+    
+    console.log(isNaN(parseFloatNew(testi1)))
+    console.log(isNaN(testi2))
+
+}
